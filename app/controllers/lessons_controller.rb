29cad1bc8973ffb20137ae0aa4ed6fca9d155cd5
Faisal <faisal.abdullah@gmail.com>
2017-03-11 07:36:12 +0000

    Permission granted to lessons for teachers and enrolled students only

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,8 +1,21 @@
 class LessonsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :require_authorized_for_current_course, only: [:show]
+
   def show
   end
 
   private
+
+  def require_authorized_for_current_course
+    if current_course.user != current_user && ! current_user.enrolled_in?(current_course)
+    redirect_to course_path(current_course), alert: 'You are not enrolled in this course'
+    end
+  end
+
+  def current_course
+  @current_course ||= current_lesson.section.course
+  end
 
   helper_method :current_lesson
   def current_lesson
